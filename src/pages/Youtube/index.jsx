23cac8d47e9c7ca610a4974317b3912e977cce59
lota <christiansolomon592@gmail.com>
2023-11-21 14:17:52 +0100

    styling header

--- src/pages/Youtube/index.jsx
+++ src/pages/Youtube/index.jsx
@@ -1,126 +1,133 @@
 import React from 'react'
-import style from "../Youtube/index.module.css"
+import video from "./video.module.css"
 import img1 from "../../assets/thumbnail-1.webp"
+import header from "./header.module.css";
 
 const Youtube = () => {
   return (
     <div>
-        <div className={style.header}>
+        <div className={header.header}>
 
-            <input className={style.search_bar} type='text' placeholder='search'/>
-            <div className={style.left_section}></div>
-            <div className={style.middle_section}></div>
-            <div className={style.right_section}></div>
+            <div className={header.left_section}>
+
+            </div>
+            <div className={header.middle_section}>
+                <input className={header.search_bar} type='text' placeholder='search'/>
+
+            </div>
+            <div className={header.right_section}>
+
+            </div>
         </div>
-        <div className={style.video_grid}>
-            <div className={style.video_preview}>
-                <div className={style.video_preview_img}>
-                    <img className={style.thumbnail} src={img1}/>
+        <div className={video.video_grid}>
+            <div className={video.video_preview}>
+                <div className={video.video_preview_img}>
+                    <img className={video.thumbnail} src={img1}/>
                 </div>
-                <div className={style.video_info_grid}>
-                    <div className={style.channel_picture}>
-                        <img className={style.profile_picture} src='src\channels\channel-1.jpeg'/>
+                <div className={video.video_info_grid}>
+                    <div className={video.channel_picture}>
+                        <img className={video.profile_picture} src='src\channels\channel-1.jpeg'/>
                     </div>
-                    <div className={style.video_info}>
-                        <p className={style.video_title}>Talking Tech and AI with Senior dev Lota</p>
-                        <p className={style.video_author}>Lotachukwu Onwuka</p>
-                        <p className ={style.video_stats}>3.4M views &#183; 6 months ago </p>
-                    </div>
-                </div>
-            </div>
-
-            <div className={style.video_preview}>
-                <div className={style.video_preview_img}>
-                    <img className={style.thumbnail} src={img1}/>
-                </div>
-                <div className={style.video_info_grid}>
-                    <div className={style.channel_picture}>
-                        <img className={style.profile_picture} src='src\channels\channel-1.jpeg'/>
-                    </div>
-                    <div className={style.video_info}>
-                        <p className={style.video_title}>Talking Tech and AI with Senior dev Lota</p>
-                        <p className={style.video_author}>Lotachukwu Onwuka</p>
-                        <p className ={style.video_stats}>3.4M views &#183; 6 months ago </p>
+                    <div className={video.video_info}>
+                        <p className={video.video_title}>Talking Tech and AI with Senior dev Lota</p>
+                        <p className={video.video_author}>Lotachukwu Onwuka</p>
+                        <p className ={video.video_stats}>3.4M views &#183; 6 months ago </p>
                     </div>
                 </div>
             </div>
 
-            <div className={style.video_preview}>
-                <div className={style.video_preview_img}>
-                    <img className={style.thumbnail} src={img1}/>
+            <div className={video.video_preview}>
+                <div className={video.video_preview_img}>
+                    <img className={video.thumbnail} src="src/assets/thumbnail-2.webp"/>
                 </div>
-                <div className={style.video_info_grid}>
-                    <div className={style.channel_picture}>
-                        <img className={style.profile_picture} src='src\channels\channel-1.jpeg'/>
+                <div className={video.video_info_grid}>
+                    <div className={video.channel_picture}>
+                        <img className={video.profile_picture} src='src\channels\channel-1.jpeg'/>
                     </div>
-                    <div className={style.video_info}>
-                        <p className={style.video_title}>Talking Tech and AI with Senior dev Lota</p>
-                        <p className={style.video_author}>Lotachukwu Onwuka</p>
-                        <p className ={style.video_stats}>3.4M views &#183; 6 months ago </p>
-                    </div>
-                </div>
-            </div>
-
-            <div className={style.video_preview}>
-                <div className={style.video_preview_img}>
-                    <img className={style.thumbnail} src={img1}/>
-                </div>
-                <div className={style.video_info_grid}>
-                    <div className={style.channel_picture}>
-                        <img className={style.profile_picture} src='src\channels\channel-1.jpeg'/>
-                    </div>
-                    <div className={style.video_info}>
-                        <p className={style.video_title}>Talking Tech and AI with Senior dev Lota</p>
-                        <p className={style.video_author}>Lotachukwu Onwuka</p>
-                        <p className ={style.video_stats}>3.4M views &#183; 6 months ago </p>
+                    <div className={video.video_info}>
+                        <p className={video.video_title}>Talking Tech and AI with Senior dev Lota</p>
+                        <p className={video.video_author}>Lotachukwu Onwuka</p>
+                        <p className ={video.video_stats}>3.4M views &#183; 6 months ago </p>
                     </div>
                 </div>
             </div>
 
-            <div className={style.video_preview}>
-                <div className={style.video_preview_img}>
-                    <img className={style.thumbnail} src={img1}/>
+            <div className={video.video_preview}>
+                <div className={video.video_preview_img}>
+                    <img className={video.thumbnail} src="src/assets/thumbnail-3.webp"/>
                 </div>
-                <div className={style.video_info_grid}>
-                    <div className={style.channel_picture}>
-                        <img className={style.profile_picture} src='src\channels\channel-1.jpeg'/>
+                <div className={video.video_info_grid}>
+                    <div className={video.channel_picture}>
+                        <img className={video.profile_picture} src='src\channels\channel-1.jpeg'/>
                     </div>
-                    <div className={style.video_info}>
-                        <p className={style.video_title}>Talking Tech and AI with Senior dev Lota</p>
-                        <p className={style.video_author}>Lotachukwu Onwuka</p>
-                        <p className ={style.video_stats}>3.4M views &#183; 6 months ago </p>
-                    </div>
-                </div>
-            </div>
-
-            <div className={style.video_preview}>
-                <div className={style.video_preview_img}>
-                    <img className={style.thumbnail} src={img1}/>
-                </div>
-                <div className={style.video_info_grid}>
-                    <div className={style.channel_picture}>
-                        <img className={style.profile_picture} src='src\channels\channel-1.jpeg'/>
-                    </div>
-                    <div className={style.video_info}>
-                        <p className={style.video_title}>Talking Tech and AI with Senior dev Lota</p>
-                        <p className={style.video_author}>Lotachukwu Onwuka</p>
-                        <p className ={style.video_stats}>3.4M views &#183; 6 months ago </p>
+                    <div className={video.video_info}>
+                        <p className={video.video_title}>Talking Tech and AI with Senior dev Lota</p>
+                        <p className={video.video_author}>Lotachukwu Onwuka</p>
+                        <p className ={video.video_stats}>3.4M views &#183; 6 months ago </p>
                     </div>
                 </div>
             </div>
 
-            <div className={style.video_preview}>
-                <div className={style.video_preview_img}>
-                    <img className={style.thumbnail} src="src/assets/lil sis.webp"/>
+            <div className={video.video_preview}>
+                <div className={video.video_preview_img}>
+                    <img className={video.thumbnail} src="src/assets/thumbnail-4.webp"/>
                 </div>
-                <div className={style.video_info_grid}>
-                    <div className={style.channel_picture}>
-                        <img className={style.profile_picture} src='src\channels\channel-1.jpeg'/>
+                <div className={video.video_info_grid}>
+                    <div className={video.channel_picture}>
+                        <img className={video.profile_picture} src='src\channels\channel-1.jpeg'/>
                     </div>
-                    <div className={style.video_info}>
-                        <p className={style.video_title}>Talking Tech and AI with Senior dev Lota</p>
-                        <p className={style.video_author}>Lotachukwu Onwuka</p>
-                        <p className ={style.video_stats}>3.4M views &#183; 6 months ago </p>
+                    <div className={video.video_info}>
+                        <p className={video.video_title}>Talking Tech and AI with Senior dev Lota</p>
+                        <p className={video.video_author}>Lotachukwu Onwuka</p>
+                        <p className ={video.video_stats}>3.4M views &#183; 6 months ago </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={video.video_preview}>
+                <div className={video.video_preview_img}>
+                    <img className={video.thumbnail} src="src/assets/thumbnail-5.webp"/>
+                </div>
+                <div className={video.video_info_grid}>
+                    <div className={video.channel_picture}>
+                        <img className={video.profile_picture} src='src\channels\channel-1.jpeg'/>
+                    </div>
+                    <div className={video.video_info}>
+                        <p className={video.video_title}>Talking Tech and AI with Senior dev Lota</p>
+                        <p className={video.video_author}>Lotachukwu Onwuka</p>
+                        <p className ={video.video_stats}>3.4M views &#183; 6 months ago </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={video.video_preview}>
+                <div className={video.video_preview_img}>
+                    <img className={video.thumbnail} src="src/assets/thumbnail-6.webp"/>
+                </div>
+                <div className={video.video_info_grid}>
+                    <div className={video.channel_picture}>
+                        <img className={video.profile_picture} src='src\channels\channel-1.jpeg'/>
+                    </div>
+                    <div className={video.video_info}>
+                        <p className={video.video_title}>Talking Tech and AI with Senior dev Lota</p>
+                        <p className={video.video_author}>Lotachukwu Onwuka</p>
+                        <p className ={video.video_stats}>3.4M views &#183; 6 months ago </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={video.video_preview}>
+                <div className={video.video_preview_img}>
+                    <img className={video.thumbnail} src="src/assets/lil sis.webp"/>
+                </div>
+                <div className={video.video_info_grid}>
+                    <div className={video.channel_picture}>
+                        <img className={video.profile_picture} src='src\channels\channel-1.jpeg'/>
+                    </div>
+                    <div className={video.video_info}>
+                        <p className={video.video_title}>Talking Tech and AI with Senior dev Lota</p>
+                        <p className={video.video_author}>Lotachukwu Onwuka</p>
+                        <p className ={video.video_stats}>3.4M views &#183; 6 months ago </p>
                     </div>
                 </div>
             </div>
