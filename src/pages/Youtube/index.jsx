1114f11b0322f9e8f3d2c48af63b75b231bcd4af
lota <christiansolomon592@gmail.com>
2023-11-23 17:49:36 +0100

    styling side bar

--- src/pages/Youtube/index.jsx
+++ src/pages/Youtube/index.jsx
@@ -34,23 +34,29 @@ const Youtube = () => {
         </div>
 
         <div className={sidebar.sidebars}>
-            <div>
+            <div className={sidebar.sidebars_box}>
                 <img className={sidebar.home} src="src/sidebars/home.svg"/>
+                <div>Home</div>
             </div>
-            <div>
+            <div className={sidebar.sidebars_box}>
                 <img className={sidebar.explore} src="src/sidebars/explore.svg"/>
+                <div>Explore</div>
             </div>
-            <div>
+            <div className={sidebar.sidebars_box}>
                 <img className={sidebar.subscriptions} src="src/sidebars/subscriptions.svg"/>
+                <div>Subscriptions</div>
             </div>
-            <div>
+            <div className={sidebar.sidebars_box}>
                 <img className={sidebar.originals} src="src/sidebars/originals.svg"/>
+                <div>Originals</div>
             </div>
-            <div>
+            <div className={sidebar.sidebars_box}>
                 <img className={sidebar.youtube_music} src="src/sidebars/youtube-music.svg"/>
+                <div>YouTube Music</div>
             </div>
-            <div>
+            <div className={sidebar.sidebars_box}>
                 <img className={sidebar.library} src="src/sidebars/library.svg"/>
+                <div>Library</div>
             </div>
         </div>
         <div className={video.video_grid}>
