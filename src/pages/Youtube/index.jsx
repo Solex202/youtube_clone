4f9fe88aa641e0fb4a43f0247654ebfbed0fb792
lota <christiansolomon592@gmail.com>
2023-11-27 12:21:48 +0100

    putting divs in separate components

--- src/pages/Youtube/index.jsx
+++ src/pages/Youtube/index.jsx
@@ -1,79 +1,14 @@
 // import React from 'react'
 import video from "./video.module.css"
 import img1 from "../../assets/thumbnail-1.webp"
-import header from "./header.module.css";
-import sidebar from "./sidebar.module.css";
+import Sidebar from "./sidebar.jsx";
+import Header from "./header.jsx";
 
 const Youtube = () => {
   return (
     <div>
-        <div className={header.headers}>
-
-            <div className={header.left_section}>
-                <img className={header.hamburger_menu} src="src/icons/hamburger-menu.svg"/>
-                <img className={header.youtube_logo} src="src/icons/youtube-logo.svg"/>
-            </div>
-            <div className={header.middle_section}>
-                <input className={header.search_bar} type='text' placeholder='search'/>
-                <button className={header.search_button}>
-                    <img  className={header.search_icon} src="src/icons/search.svg"/>
-                    <div className={header.tooltip}>Search</div>
-                </button>
-                <button className={header.voice_search}>
-                    <img className={header.voice_search_icon} src="src/icons/voice-search-icon.svg"/>
-                    <div className={header.tooltip}>Search with your voice</div>
-                </button>
-            </div>
-            <div className={header.right_section}>
-                <div className={header.upload_icon_container}>
-                    <img className={header.upload_icon} src="src/icons/upload.svg"/>
-                    <div className={header.tooltip}>Create</div>
-                </div>
-                <div className={header.youtube_apps_icon_container}>
-                    <img className={header.youtube_apps_icon} src="src/icons/youtube-apps.svg"/>
-                    <div className={header.tooltip}>Youtube apps</div>
-
-                </div>
-                <div className={header.notification_icon_container}>
-                    <img className={header.notifications_icon} src="src/icons/notifications.svg"/>
-                    <div className={header.notifications_count}>3</div>
-                    <div className={header.tooltip}>Notifications</div>
-                </div>
-                <img className={header.profile_picture} src="src/channels/channel-1.jpeg"/>
-            </div>
-        </div>
-
-
-
-        <div className={sidebar.sidebars}>
-            <div className={sidebar.sidebars_box}>
-                <img className={sidebar.home} src="src/sidebars/home.svg"/>
-                <div>Home</div>
-            </div>
-            <div className={sidebar.sidebars_box}>
-                <img className={sidebar.explore} src="src/sidebars/explore.svg"/>
-                <div>Explore</div>
-            </div>
-            <div className={sidebar.sidebars_box}>
-                <img className={sidebar.subscriptions} src="src/sidebars/subscriptions.svg"/>
-                <div>Subscriptions</div>
-            </div>
-            <div className={sidebar.sidebars_box}>
-                <img className={sidebar.originals} src="src/sidebars/originals.svg"/>
-                <div>Originals</div>
-            </div>
-            <div className={sidebar.sidebars_box}>
-                <img className={sidebar.youtube_music} src="src/sidebars/youtube-music.svg"/>
-                <div>YouTube Music</div>
-            </div>
-            <div className={sidebar.sidebars_box}>
-                <img className={sidebar.library} src="src/sidebars/library.svg"/>
-                <div>Library</div>
-            </div>
-        </div>
-
-
-
+        <Header/>
+        <Sidebar/>
         <div className={video.video_grid}>
             <div className={video.video_preview} >
                 <a className={video.link} href='https://www.youtube.com/watch?v=31SUabPZY1w'>
